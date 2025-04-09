c698288886325e4acd5d8d380d617495ef39b1a0
Justin Mendillo <justinmendillo.gmail.com>
2025-04-08 17:06:19 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+
+// function fibonacci(n) 
+// {
+//    if (n <= 0) return 0;
+//    if (n === 1) return 1;
+    
+//    let a = 0, b = 1;
+//    for (let i = 2; i <= n; i++) {
+//        let temp = a + b;
+//        a = b;
+//        b = temp;
+//    }
+//    return b;
+//}
+
+fn fibonacci(value: i32) -> i32
+{
+    if value <= 0 
+    {
+        return 0;
+    }
+    else if value == 1
+    {
+        return 1;
+    }
+
+    let mut a = 0;
+    let mut b = 1;
+
+    for _i in 2..=value
+    {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    b
+}
+
+fn main()
+{
+    println!("The value is {}.", fibonacci(3));
+}
